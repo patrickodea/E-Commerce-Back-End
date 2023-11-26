a896bd57a20b5c2ddfb63caebdb7e19551a19b61
Patrick O'Dea <po39dea@gmail.com>
2023-11-26 16:33:14 -0700

    Set up category model rules and fields

---
+++ models/Category.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequalize = require("../config/connection.js");
+
+class Category extends Model {}
+
+Category.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        category_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequalize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "category",
+    }
+);
+
+module.exports = Category;
