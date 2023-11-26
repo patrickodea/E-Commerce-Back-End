fff18bf21cbee372b57af01cf8cf6507f15857e0
Patrick O'Dea <po39dea@gmail.com>
2023-11-26 16:39:19 -0700

    Set up product model fields and rules

---
+++ models/Product.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequalize = require("../config/connection.js");
+
+class Product extends Model {}
+
+Product.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+            isDecimal: true,
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            isNumeric: true,
+            defaultValue: 10,
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "category",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequalize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "product",
+    }
+);
+
+module.exports = Product;
