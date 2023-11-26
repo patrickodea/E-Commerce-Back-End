1887e9b2c813d43a6bc90e86c21a1e8764645b03
Patrick O'Dea <po39dea@gmail.com>
2023-11-26 16:46:40 -0700

    Set up tag model fields and rules

---
+++ models/Tag.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequalize = require("../config/connection.js");
+
+class Tag extends Model {}
+
+Tag.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        tag_name: {
+            type: DataTypes.STRING,
+        },
+    },
+    {
+        sequalize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "tag",
+    }
+);
+
+module.exports = Tag;
