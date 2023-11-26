2c81a8d5c71a0bf7151c5ad054fe64d9fe5d51e6
Patrick O'Dea <po39dea@gmail.com>
2023-11-26 16:53:29 -0700

    Set up models index for exporting

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+
+Product.belongsTo(Category,{
+    foreignKey: "category_id",
+    onDelete: "CASCADE",
+});
+
+Category.hasMany(Product, {
+    foreignKey: "category_id",
+    onDelete: "CASCADE",
+});
+
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    foreignKey: "product_id",
+    onDelete: "CASCADE",
+});
+
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    foreignKey: "tag_id",
+    onDelete: "CASCADE",
+});
+
+module.exports = {
+    Product,
+    Category,
+    Tag,
+    ProductTag,
+};
