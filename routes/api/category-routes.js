a09323c3c838c3bd5b569fd788b2a5fcd853b72c
Patrick O'Dea <po39dea@gmail.com>
2023-11-26 19:59:51 -0700

    Created category routes and category seeds

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,4 +19,60 @@ router.get("/", (req, res) =>{
     });
 });
 
-router.get("/:id", (req, res) =>)
+router.get("/:id", (req, res) =>{
+    Category.findOne({
+        where: [
+            {
+                model: Product,
+                attributes: ["id", "product_name", "price", "stock", "category_id"],
+            },
+        ],
+    })
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+    console.log(req.body);
+    Category.create({ category_name: req.body.category_name })
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+    Category.update(req.body, {
+        where: {
+            id: req.params.id,
+        },
+    })
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+    Category.destroy({
+        where: {
+            id: req.params.id,
+        },
+    })
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    });
+});
+
+module.exports = router;
